34662599808f97f1a4895127137aa3b42634980f
Timotej <timotej@maxborsky.com>
2024-07-10 01:04:51 +0200

    Add Language Select

---
+++ src-tauri/frontend/intro.js
@@ -0,0 +1,9 @@
+document.getElementById('continueEnglishBtn').addEventListener('click', function() {
+  // Redirect to the next page for English language
+  window.location.href = 'mainpage-en.html'; // Replace with actual URL of the next page in English
+});
+
+document.getElementById('continueCzechBtn').addEventListener('click', function() {
+  // Redirect to the next page for Czech language
+  window.location.href = 'mainpage-cz.html'; // Replace with actual URL of the next page in Czech
+});
