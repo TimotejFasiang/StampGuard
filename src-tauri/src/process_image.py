dd0cb241bb85faf3688e460fa10074cae9eb8405
Timotej <timotej@maxborsky.com>
2024-07-23 00:54:21 +0200

    Add debug print statement

--- src-tauri/src/process_image.py
+++ src-tauri/src/process_image.py
@@ -2,9 +2,10 @@ import sys
 import cv2
 
 if __name__ == "__main__":
+    print("Running process_image.py")
     image_path = sys.argv[1]
     original_image = cv2.imread(image_path)
     flipped_image = cv2.flip(original_image, 1)
-    output_path = '/home/timotej/PycharmProjects/StampGuard/src-tauri/frontend/processed_image.jpg'
+    output_path = './frontend/processed_image.jpg'
     cv2.imwrite(output_path, flipped_image)
 
