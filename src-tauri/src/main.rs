3f76888a98852cc28db9d5ec31c0cb3a1954edc7
Timotej <timotej@maxborsky.com>
2024-07-23 00:55:01 +0200

    Add command for "save_image" python file

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,6 +2,20 @@ use std::process::Command;
 use tauri::command;
 
 #[command]
+fn save_image(message: String) {
+    let python_script_path = "./src/save_image.py";
+
+    let output = Command::new("python3")
+        .arg(python_script_path)
+        .arg(&message)  // Pass the selected file path as an argument to Python script
+        .output()
+        .expect("Failed to execute Python script");
+
+    let output_str = String::from_utf8_lossy(&output.stdout);
+    println!("{}", output_str); // Print any python output
+}
+
+#[command]
 fn process_image(message: String) {
     let python_script_path = "./src/process_image.py";
 
@@ -18,6 +32,7 @@ fn process_image(message: String) {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![process_image])
+        .invoke_handler(tauri::generate_handler![save_image])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
