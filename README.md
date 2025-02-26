7c78f6392c9e0e6e3ce852ad523f025b96b56ff9
timotejfasiang <timotej@maxborsky.com>
2025-02-26 09:18:57 +0100

    Transfer to Tauri V2

---
+++ README.md
@@ -0,0 +1,41 @@
+# StampGuard
+
+StampGuard is a cross-platform desktop application designed to detect fraud and fake historical Czechoslovak stamps. It utilizes algorithms to analyze and verify the authenticity of stamps based on various criteria.
+
+## Features
+
+- **Stamp Image Analysis:** Upload a photo of a stamp to analyze its authenticity.
+- **Advanced Algorithms:** Utilizes advanced models and image processing algorithms to detect patterns indicative of fraudulent or fake stamps.
+- **User-friendly Interface:** Simple and intuitive UI for easy operation and analysis.
+- **Offline Mode:** Works entirely offline, no need for internet connectivity for stamp verification.
+- **Detailed Findings:** Provides detailed findings on the analysis and authenticity of each stamp.
+- **Customizable Settings:** Adjustable settings for different stamp types and verification thresholds.
+- **Other:** Other features.
+
+## Getting Started
+
+### Prerequisites
+
+- Windows, macOS, or Linux operating system
+- Maybe Android or IOS phone??
+- Other prerequisites
+
+### Installation
+
+1. **Visit the Releases Page**:
+   Go to the [Releases page](https://github.com/TimotejFasiang/StampGuard/releases/).
+
+2. **Download the Latest Release**:
+   Find the latest release and download the appropriate file for your operating system:
+   - For Windows: `StampGuard-x.x.x-win64.zip`
+   - For macOS: `StampGuard-x.x.x-mac.zip`
+   - For Linux: `StampGuard-x.x.x-linux.tar.gz`
+
+3. **Extract the Archive**:
+   Extract the downloaded archive to a desired location on your computer.
+
+4. **Run the Software**:
+   Navigate to the extracted folder and run the executable file:
+   - On Windows: Double-click `StampGuard.exe`
+   - On macOS: Open `StampGuard.app`
+   - On Linux: Run `./StampGuard.sh` from the terminal
---
+++ src/routes/en/introduction/+page.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+    import { open } from '@tauri-apps/plugin-dialog';
+	import { Command } from "@tauri-apps/plugin-shell";
+
+    // Function to handle file selection and invoke the command
+    async function handleFileSelection() {
+        console.log("Starting handleFileSelection")
+        const selectedFilePath = await open({
+            multiple: false,
+            directory: false,
+            title: "Select an image"
+        });
+
+        if (selectedFilePath) {
+        	console.log("Invoking save_image");
+
+			try {
+				const command = Command.sidecar('src/save_image', [selectedFilePath]);
+				const output = await command.execute();  // Executes and waits for result
+
+				console.log(`[Python stdout]: ${output.stdout}`);
+				console.error(`[Python stderr]: ${output.stderr}`);
+
+				if (output.code !== 0) {
+					console.error(`Process exited with error code: ${output.code}`);
+				}
+			} catch (error) {
+				console.error(`Command execution error: ${error}`);
+			}
+
+			console.log("Done invoking save_image");
+
+			window.location.href = '/en/select_image';
+		}
+    }
+</script>
+
+
+<style>
+    :global(body, html) {
+        margin: 0;
+	    overflow: hidden;
+	    height: 100vh;
+		width: 100vw;
+    }
+
+    .buttonContainer {
+	    display: flex;
+	    justify-content: center;
+	    align-items: center;
+	    height: 100%;
+	    width: 100%;
+	    position: absolute;
+	    overflow: hidden;
+	}
+
+    button {
+        padding: 10px 24px;
+        font-size: 18px;
+	    border-radius: 3px;
+        cursor: pointer;
+	    background-color: #000;
+        color: #fff;
+	    border: 2px solid #000;
+    }
+
+    button:hover {
+	    background-color: #fff;
+        color: #000;
+    }
+</style>
+
+<main>
+	<div class="buttonContainer">
+        <button id="uploadButton" on:click={handleFileSelection}>Select Image</button>
+	</div>
+</main>
